fb1623419918fdf4d46df1e7aa800abbb43fe3e6
Yi Wei <yiwei.in.cyber@gmail.com>
2014-04-28 09:46:26 +0800

    Container passed RSpec first time

---
+++ lib/infused.rb
@@ -0,0 +1,1 @@
+require 'infused/container'
--- lib/infused/container.rb
+++ lib/infused/container.rb
@@ -1,19 +1,28 @@
 module Infused
   class Container
+    def initialize
+      @ctors_map = {}
+    end
+    
     def register(constructor, id = nil)
       if id.nil?
-        @ctors_map[constructor.class.to_s.to_sym] = constructor
+        @ctors_map[constructor.to_s.to_sym] = constructor
       else
         @ctors_map[id] = constructor
       end
     end
 
     def instantiate(id, *args, **kwargs)
-      if @ctors_map.has_key?(id)
+      if has?(id)
         return @ctors_map[id].new(*args, **kwargs)
       else
         raise ContructorNotRegisteredError.new
       end
     end
+    
+    def has?(id)
+      @ctors_map.has_key?(id)
+    end
+    
   end
 end
